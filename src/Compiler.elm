00cafd54db1811686426c325ca88148b184659c8
Akama Hitoshi <13938897+akamah@users.noreply.github.com>
2023-05-02 22:39:50 +0900

    move examples to compiler section

--- src/Compiler.elm
+++ src/Compiler.elm
@@ -1,8 +1,44 @@
 module Compiler exposing (compile)
 
+import Dir
+import Joint
+import Kind
 import Rail exposing (Rail)
+import Rot45
+import Tie
+
+
+test1 : List Rail
+test1 =
+    [ Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 0 Dir.e Joint.plus
+    , Rail Kind.CurveLeft <|
+        Tie.make (Rot45.make 1 0 0 0) Rot45.zero 0 Dir.e Joint.plus
+    , Rail Kind.CurveLeft <|
+        Tie.make (Rot45.make 1 0 1 -1) Rot45.zero 0 Dir.ne Joint.plus
+    , Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 4 Dir.e Joint.plus
+    , Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 8 Dir.e Joint.plus
+    , Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 12 Dir.e Joint.plus
+    , Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 16 Dir.e Joint.plus
+    , Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 20 Dir.e Joint.plus
+    , Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 24 Dir.e Joint.plus
+    , Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 28 Dir.e Joint.plus
+    , Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 32 Dir.e Joint.plus
+    , Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 36 Dir.e Joint.plus
+    , Rail Kind.Straight <|
+        Tie.make Rot45.zero Rot45.zero 40 Dir.e Joint.plus
+    ]
 
 
 compile : String -> List Rail
 compile _ =
-    Rail.test1
+    test1
--- src/Rail.elm
+++ src/Rail.elm
@@ -1,4 +1,4 @@
-module Rail exposing (Rail, make, test1)
+module Rail exposing (Rail, make)
 
 import Dir
 import Joint
@@ -18,34 +18,3 @@ make kind origin =
     { kind = kind
     , origin = origin
     }
-
-
-test1 : List Rail
-test1 =
-    [ Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 0 Dir.e Joint.plus
-    , Rail Kind.Curve <|
-        Tie.make (Rot45.make 1 0 0 0) Rot45.zero 0 Dir.e Joint.plus
-    , Rail Kind.Curve <|
-        Tie.make (Rot45.make 1 0 1 -1) Rot45.zero 0 Dir.ne Joint.plus
-    , Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 4 Dir.e Joint.plus
-    , Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 8 Dir.e Joint.plus
-    , Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 12 Dir.e Joint.plus
-    , Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 16 Dir.e Joint.plus
-    , Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 20 Dir.e Joint.plus
-    , Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 24 Dir.e Joint.plus
-    , Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 28 Dir.e Joint.plus
-    , Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 32 Dir.e Joint.plus
-    , Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 36 Dir.e Joint.plus
-    , Rail Kind.Straight <|
-        Tie.make Rot45.zero Rot45.zero 40 Dir.e Joint.plus
-    ]
