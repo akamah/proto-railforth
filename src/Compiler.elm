9cdf6406b74024c2463fd7158dda648425cf0492
Akama Hitoshi <13938897+akamah@users.noreply.github.com>
2023-05-02 22:39:51 +0900

    create composition of tie

--- src/Compiler.elm
+++ src/Compiler.elm
@@ -2,7 +2,7 @@ module Compiler exposing (compile)
 
 import Dir
 import Joint
-import Kind
+import Kind exposing (Kind(..))
 import Rail exposing (Rail)
 import Rot45
 import Tie exposing (Tie)
@@ -39,6 +39,19 @@ test1 =
     ]
 
 
+getLocalTie : Kind -> Tie
+getLocalTie k =
+    case k of
+        Straight ->
+            Tie.make (Rot45.make 1 0 0 0) Rot45.zero 0 Dir.e Joint.plus
+
+        CurveLeft ->
+            Tie.make (Rot45.make 0 0 1 -1) Rot45.zero 0 Dir.ne Joint.plus
+
+        CurveRight ->
+            Tie.make (Rot45.make 0 1 -1 0) Rot45.zero 0 Dir.se Joint.plus
+
+
 goStraight : Tie -> Tie
 goStraight tie =
     let
@@ -58,6 +71,30 @@ curveLeft tie =
             Rot45.mul Dir.ne tie.dir
     in
     Tie.make ns tie.double tie.height nd tie.joint
+
+
+getNextTie : Tie -> Kind -> Tie
+getNextTie tie kind =
+    let
+        local =
+            getLocalTie kind
+
+        single2 =
+            Rot45.add tie.single <| Rot45.mul tie.dir local.single
+
+        double2 =
+            Rot45.add tie.double <| Rot45.mul tie.dir local.double
+
+        height2 =
+            tie.height + local.height
+
+        dir2 =
+            Rot45.mul tie.dir local.dir
+
+        joint2 =
+            Rot45.mul tie.joint local.joint
+    in
+    Tie.make single2 double2 height2 dir2 joint2
 
 
 tokenize : String -> List String
@@ -83,10 +120,13 @@ compileRec tie toks =
         t :: ts ->
             case t of
                 "s" ->
-                    Rail.make Kind.Straight tie :: compileRec (goStraight tie) ts
+                    Rail.make Straight tie :: compileRec (getNextTie tie Straight) ts
 
                 "l" ->
-                    Rail.make Kind.CurveLeft tie :: compileRec (curveLeft tie) ts
+                    Rail.make CurveLeft tie :: compileRec (getNextTie tie CurveLeft) ts
+
+                "r" ->
+                    Rail.make CurveRight tie :: compileRec (getNextTie tie CurveRight) ts
 
                 _ ->
                     []
