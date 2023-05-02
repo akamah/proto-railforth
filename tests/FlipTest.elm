89773bbcb2d92c099ee6e7f8b6002ce8ca013943
Akama Hitoshi <13938897+akamah@users.noreply.github.com>
2023-05-02 22:39:51 +0900

    write some test

---
+++ tests/FlipTest.elm
@@ -0,0 +1,29 @@
+module FlipTest exposing (..)
+
+import Expect exposing (Expectation)
+import Flip exposing (..)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Flip module"
+        [ test "is true to yes" <|
+            \_ ->
+                isFlipped yes
+                    |> Expect.true "isFlipped yes"
+        , test "is false to no" <|
+            \_ ->
+                isFlipped no
+                    |> Expect.false "isFlipped no"
+        , test "yes is not no" <|
+            \_ ->
+                yes /= no |> Expect.true "yes /= no"
+        , test "toString yes" <|
+            \_ ->
+                Expect.equal "Flip@yes" (toString yes)
+        , test "toString no" <|
+            \_ ->
+                Expect.equal "Flip@no" (toString no)
+        ]
