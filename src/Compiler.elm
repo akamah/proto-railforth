1c9b8f5d6aafb38949ee4b98293950df69b97453
Akama Hitoshi <13938897+akamah@users.noreply.github.com>
2023-05-02 22:39:50 +0900

    implement go straight and curve left

--- src/Compiler.elm
+++ src/Compiler.elm
@@ -5,7 +5,7 @@ import Joint
 import Kind
 import Rail exposing (Rail)
 import Rot45
-import Tie
+import Tie exposing (Tie)
 
 
 test1 : List Rail
@@ -39,6 +39,54 @@ test1 =
     ]
 
 
+goStraight : Tie -> Tie
+goStraight tie =
+    let
+        sn =
+            Rot45.add tie.single <| tie.dir
+    in
+    Tie.make sn tie.double tie.height tie.dir tie.joint
+
+
+curveLeft : Tie -> Tie
+curveLeft tie =
+    let
+        ns =
+            Rot45.add tie.single <| Rot45.mul tie.dir (Rot45.make 0 0 1 -1)
+
+        nd =
+            Rot45.mul Dir.ne tie.dir
+    in
+    Tie.make ns tie.double tie.height nd tie.joint
+
+
+tokenize : String -> List String
+tokenize =
+    String.words
+
+
 compile : String -> List Rail
-compile _ =
-    test1
+compile src =
+    let
+        tokens =
+            tokenize src
+    in
+    compileRec (Tie.make Rot45.zero Rot45.zero 0 Dir.e Joint.plus) tokens
+
+
+compileRec : Tie -> List String -> List Rail
+compileRec tie toks =
+    case toks of
+        [] ->
+            []
+
+        t :: ts ->
+            case t of
+                "s" ->
+                    Rail.make Kind.Straight tie :: compileRec (goStraight tie) ts
+
+                "l" ->
+                    Rail.make Kind.CurveLeft tie :: compileRec (curveLeft tie) ts
+
+                _ ->
+                    []
