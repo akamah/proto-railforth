225c2742e03c4620d84470b3378edec621cf03f8
Akama Hitoshi <13938897+akamah@users.noreply.github.com>
2024-09-01 20:50:26 +0900

    Revert "remove off parser"
    This reverts commit e7784c0dde20fca07c4b0a8d8795a9b430379adf.

---
+++ tests/Graphics/OFFTest.elm
@@ -0,0 +1,84 @@
+module Graphics.OFFTest exposing (..)
+
+import Expect exposing (FloatingPointTolerance(..))
+import Graphics.OFF as OFF
+import Math.Vector3 exposing (vec3)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "OFF"
+        [ test "parse empty OFF file" <|
+            \_ ->
+                Expect.equal
+                    (Ok { vertices = [], indices = [] })
+                    (OFF.parse <|
+                        String.join "\n"
+                            [ "OFF 0 0 0"
+                            ]
+                    )
+        , test "parse triangle" <|
+            \_ ->
+                Expect.equal
+                    (Ok
+                        { vertices =
+                            [ vec3 0 0 0
+                            , vec3 1 0 0
+                            , vec3 0 1 0
+                            , vec3 0 0 1
+                            ]
+                        , indices =
+                            [ ( 0, 1, 2 )
+                            , ( 0, 1, 3 )
+                            , ( 1, 2, 3 )
+                            , ( 2, 0, 3 )
+                            ]
+                        }
+                    )
+                    (OFF.parse <|
+                        String.join "\n"
+                            [ "OFF 4 4 6"
+                            , "0 0 0"
+                            , "1 0 0"
+                            , "0 1 0"
+                            , "0 0 1"
+                            , "3 0 1 2"
+                            , "3 0 1 3"
+                            , "3 1 2 3"
+                            , "3 2 0 3"
+                            ]
+                    )
+        , test "parse triangle but nv and nf are in the next line" <|
+            \_ ->
+                Expect.equal
+                    (Ok
+                        { vertices =
+                            [ vec3 0 0 0
+                            , vec3 1 0 0
+                            , vec3 0 1 0
+                            , vec3 0 0 1
+                            ]
+                        , indices =
+                            [ ( 0, 1, 2 )
+                            , ( 0, 1, 3 )
+                            , ( 1, 2, 3 )
+                            , ( 2, 0, 3 )
+                            ]
+                        }
+                    )
+                    (OFF.parse <|
+                        String.join "\n"
+                            [ "OFF"
+                            , "4 4 6"
+                            , "0 0 0"
+                            , "1 0 0"
+                            , "0 1 0"
+                            , "0 0 1"
+                            , "3 0 1 2"
+                            , "3 0 1 3"
+                            , "3 1 2 3"
+                            , "3 2 0 3"
+                            ]
+                    )
+        ]
