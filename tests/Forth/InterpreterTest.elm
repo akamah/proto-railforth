a9d355550990fcf3aabbddcb4cc59e60561d155f
Akama Hitoshi <13938897+akamah@users.noreply.github.com>
2024-12-20 23:33:00 +0900

    update unit tests

--- tests/Forth/InterpreterTest.elm
+++ tests/Forth/InterpreterTest.elm
@@ -5,32 +5,49 @@ import Forth.Interpreter exposing (execute)
 import Test exposing (..)
 
 
+equal : List String -> List String -> Expectation
+equal expected actual =
+    Expect.equal
+        (execute <| String.join "\n" expected)
+        (execute <| String.join "\n" actual)
+
+
 suite : Test
 suite =
     describe "Interpreter module"
         [ test "define word" <|
             \_ ->
-                Expect.equal
-                    (execute ": foo l r s r l ; foo")
-                    (execute "l r s r l")
-        , test "word and variable" <|
+                equal [ "s s" ]
+                    [ ": foo s s ; foo" ]
+        , test "calling word inside other word def" <|
             \_ ->
-                Expect.equal
-                    (execute ": foo l r ; tr save hoge foo . load hoge s")
-                    (execute "tr swap l r . s")
-        , test "nestedword" <|
+                equal [ "l r l r" ]
+                    [ ": foo l r ;"
+                    , ": bar foo foo ;"
+                    , "bar"
+                    ]
+        , test "define word inside word def" <|
             \_ ->
-                Expect.equal
-                    (execute ": foo l r ; : bar foo foo ; bar")
-                    (execute "l r l r")
-        , test "comment" <|
-            \_ ->
-                Expect.equal
-                    (execute "s s ( comment line ( dayo ) ) s s")
-                    (execute "s s s s")
+                equal [ "l r l r" ]
+                    [ ": foo"
+                    , "  : bar l r ;"
+                    , "  bar bar"
+                    , ";"
+                    , "foo"
+                    ]
         , test "save and load" <|
             \_ ->
-                Expect.equal
-                    (execute "tr save foo s . load foo l .")
-                    (execute "tr swap s . l .")
+                equal [ "tr swap s . l ." ]
+                    [ "tr save foo s . load foo l ."
+                    ]
+        , test "word and variable" <|
+            \_ ->
+                equal [ "tr swap l r . s" ]
+                    [ ": foo l r ;"
+                    , "tr save hoge foo . load hoge s"
+                    ]
+        , test "comment" <|
+            \_ ->
+                equal [ "s s s s" ]
+                    [ "s s ( comment line ( dayo ) ) s s" ]
         ]
